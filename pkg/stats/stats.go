04e56d50220ca5944ab47ffa0c0e4abbea4c01ec
Fanisa <fanisa_1194@mail.ru>
2020-09-29 11:25:46 +0500

    проверка оплаты по категориям

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,15 +1,17 @@
 package stats
 
 import (
+	"reflect"
+	"testing"
 	"github.com/Fanisabonu/bank/v2/pkg/types"
 )
 
 //Avg -
-func Avg(payments []types.Payment) (money types.Money){
-	c :=0 
+func Avg(payments []types.Payment) (money types.Money) {
+	c := 0
 	for _, payment := range payments {
 		if payment.Status != types.StatusFail {
-			c++ 
+			c++
 			money += payment.Amount
 		}
 	}
@@ -27,3 +29,56 @@ func TotalInCategory(payments []types.Payment, category types.Category) (money t
 	}
 	return
 }
+func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
+	moneys := map[types.Category]types.Money{}
+	c := map[types.Category]types.Money{}
+	for _, payment := range payments {
+		if payment.Status != types.StatusFail {
+			moneys[payment.Category] += payment.Amount
+			c[payment.Category]++
+		}
+	}
+	for key:= range moneys  {
+		moneys[key]=moneys[key]/c[key]
+	}
+	return moneys 
+}
+
+func TestCategoriesAvg_manyCategory(t *testing.T) {
+	payments := []types.Payment{
+	  {
+		Category: "car",
+		Amount:   0,
+		Status:   types.StatusOk,
+	  },
+	  {
+		Category: "car",
+		Amount:   100,
+		Status:   types.StatusInProgress,
+	  },
+	  {
+		Category: "food",
+		Amount:   10000,
+		Status:   types.StatusOk,
+	  },
+	  {
+		Category: "fun",
+		Amount:   100,
+		Status:   types.StatusOk,
+	  },
+	  {
+		Category: "fun",
+		Amount:   100,
+		Status:   types.StatusFail,
+	  },
+	}
+	want := map[types.Category]types.Money{
+	  "car":  50,
+	  "food": 10000,
+	  "fun":  100,
+	}
+	got := CategoriesAvg(payments)
+	if !reflect.DeepEqual(want, got) {
+	  t.Errorf("want: %v, got: %v", want, got)
+	}
+  }
